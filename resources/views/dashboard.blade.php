958c0db44c631170137c11486fe3d6932db6a602
jazzankan <shasfn@gmail.com>
2021-01-26 16:48:26 +0100

    Börjat med proj view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,29 +1,27 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <img class="inline rounded" src="https://webbsallad.se/ankfiles/redduck100.png"><span class="ml-4 text-3xl text-red-900">Ankhemmet</span>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="max-w-lg bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200 bg-green-100">
                     <h4><a class="text-blue-600 text-xl font-bold hover:text-purple-600 hover:underline" href="/memories">Minnen</a></h4>
-                    <hr class="mt-4">
+                    <hr class="hrdash">
                     <h5 class="text-lg font-semibold">Projekt</h5>
-                    <p><a href="/projects">Mina projekt</a></p>
-                    <p><a href="/todos">Ofärdiga arbetsuppgifter</a></p>
-                    <hr>
-                    <h5>Blogg</h5>
-                    <p><a href="/blog">Publik blogg</a></p>
-                    <p><a href="/articles">Blogginlägg</a> - skapa och redigera</p>
-                    <p><a href="/comments">Nya bloggkommentarer</a></p>
-                    <p><a href="/categories">Bloggkategorier</a> - skapa och redigera</p>
-                    <p><a href="/about">Om Ankhemmet</a> - lite info för bloggbesökare</p>
+                    <p><a class="dashlink" href="/projects">Mina projekt</a></p>
+                    <p><a class="dashlink" href="/todos">Ofärdiga arbetsuppgifter</a></p>
+                    <hr class="hrdash">
+                    <h5 class="text-lg font-semibold">Blogg</h5>
+                    <p><a class="dashlink" href="/blog">Publik blogg</a></p>
+                    <p><a class="dashlink" href="/articles">Blogginlägg</a> - skapa och redigera</p>
+                    <p><a class="dashlink" href="/comments">Nya bloggkommentarer</a></p>
+                    <p><a class="dashlink" href="/categories">Bloggkategorier</a> - skapa och redigera</p>
+                    <p><a class="dashlink" href="/about">Om Ankhemmet</a> - lite info för bloggbesökare</p>
 
-                    <hr>
-                    Du är inloggad!
+                    <hr class="hrdash">
+                    <p class="text-sm">Du är inloggad!</p>
                 </div>
             </div>
         </div>
